6ab74814ada93865673c1f03e20ba01d5a772915
Yan <134351027+Yanmaiscedo@users.noreply.github.com>
2025-03-16 22:35:40 -0300

    Feat: Draw function

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -32,6 +32,51 @@ void Setup()
     score = 0;
 }
 
+void Draw()
+{
+    system("cls");
+    cout << "!!!!slither.io!!!!" << endl;
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j == 0)
+                cout << "#";
+            if (i == y && j == x)
+                cout << "Q";
+            else if (i == fruitY && j == fruitX)
+                cout << "$";
+            else
+            {
+                bool print = false;
+                for (int k = 0; k < nTail; k++)
+                {
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        cout << "o";
+                        print = true;
+                    }
+                }
+                if (!print)
+                {
+                    cout << " ";
+                }
+            }
+            if (j == width - 1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+    cout << "Score: " << score << endl;
+}
+
 int main()
 {
 
