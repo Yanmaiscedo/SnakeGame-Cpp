8458b261f55c5b5229c7c34efe9256ce7d8d65e0
Yan <134351027+Yanmaiscedo@users.noreply.github.com>
2025-03-16 22:35:03 -0300

    Feat: Variables and Setup function

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -4,6 +4,34 @@
 
 using namespace std;
 
+bool gameOver;
+const int width = 40;
+const int height = 27;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+
+enum eDirection
+{
+    STOP = 0,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
+eDirection dir;
+
+void Setup()
+{
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+}
+
 int main()
 {
 
